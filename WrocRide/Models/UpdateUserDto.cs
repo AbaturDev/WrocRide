30b66099c004a06ff9349f3e83ab5eab6d0c46d2
Jan Galewicz <jan.galewicz@o2.pl>
2024-12-14 15:14:29 +0100

    feat: Add UpdateProfile in UserService to allow users to update their profiles. Add UpdateUserDto.

--- WrocRide/Models/UpdateUserDto.cs
+++ WrocRide/Models/UpdateUserDto.cs
@@ -6,6 +6,6 @@
         public string Surename { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
     }
 }
--- WrocRide/Services/UserService.cs
+++ WrocRide/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using WrocRide.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using WrocRide.Entities;
 using WrocRide.Exceptions;
 using WrocRide.Models;
 
@@ -7,15 +8,18 @@ namespace WrocRide.Services
     public interface IUserService
     {
         UserDto GetUser();
+        void UpdateUser(UpdateUserDto dto);
     }
     public class UserService : IUserService
     {
         private readonly WrocRideDbContext _dbContext;
         private readonly IUserContextService _userContextService;
-        public UserService(WrocRideDbContext dbContext, IUserContextService userContextService)
+        private readonly IPasswordHasher<User> _passwordHasher;
+        public UserService(WrocRideDbContext dbContext, IUserContextService userContextService, IPasswordHasher<User> passwordHasher)
         {
             _dbContext = dbContext;
             _userContextService = userContextService;
+            _passwordHasher = passwordHasher;
         }
 
         public UserDto GetUser()
@@ -29,11 +33,6 @@ namespace WrocRide.Services
 
             var user = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
 
-            if (user == null)
-            {
-                throw new NotFoundException("User not found"); // Change it to NotLoggedIn error
-            }
-
             var result =  new UserDto()
             {
                 Name = user.Name,
@@ -46,5 +45,46 @@ namespace WrocRide.Services
             return result;
         }
 
+        public void UpdateUser(UpdateUserDto dto)
+        {
+            int? userId = _userContextService.GetUserId;
+
+            if (userId == null)
+            {
+                throw new NotFoundException("User not found"); // Change it to NotLoggedIn error
+            }
+
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (!string.IsNullOrEmpty(dto.Name))
+            {
+                user.Name = dto.Name;
+            }
+
+            if (!string.IsNullOrEmpty(dto.Surename))
+            {
+                user.Surename = dto.Surename;
+            }
+
+            if (!string.IsNullOrEmpty(dto.Email))
+            {
+                user.Email = dto.Email;
+            }
+
+            if (!string.IsNullOrEmpty(dto.PhoneNumber))
+            {
+                user.PhoneNumber = dto.PhoneNumber;
+            }
+
+            if (!string.IsNullOrEmpty(dto.Password))
+            {
+                var hashedPassword = _passwordHasher.HashPassword(user, dto.Password);
+                user.PasswordHash = hashedPassword;
+            }
+
+            _dbContext.SaveChanges();
+
+            return;
+        }
     }
 }
