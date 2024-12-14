1984542e1893bbae081719c7e6b41c3317e6a1f5
Jan Galewicz <jan.galewicz@o2.pl>
2024-12-14 14:07:43 +0100

    feat: Add UserService and UserController. Implement GetUser in UserService to get information about logged user. Add UserDTO and UpdateUserDto

---
+++ WrocRide/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WrocRide.Models
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Surename { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime JoinAt { get; set; }
+    }
+}
