bd2fc023d91ba46f6a83a26f5008f2475ac0a6c7
Jan Galewicz <jan.galewicz@o2.pl>
2024-12-15 15:52:54 +0100

    feat: Add UpdateUserDtoValidator. Add authorization to UserControler. fix: Remove comments and unnecesery null check

--- WrocRide/Controllers/UserController.cs
+++ WrocRide/Controllers/UserController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using WrocRide.Entities;
 using WrocRide.Models;
 using WrocRide.Services;
 
 namespace WrocRide.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/me")]
     public class UserController : ControllerBase
@@ -20,8 +22,6 @@ namespace WrocRide.Controllers
         public ActionResult<UserDto> Get()
         {
             var result = _userService.GetUser();
-
-            if (result == null) { return NotFound(); }
 
             return Ok(result);
         }
---
+++ WrocRide/Models/Validators/UpdateUserDtoValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using WrocRide.Entities;
+
+namespace WrocRide.Models.Validators
+{
+    public class UpdateUserDtoValidator : AbstractValidator<UpdateUserDto>
+    {
+        public UpdateUserDtoValidator(WrocRideDbContext dbContext)
+        {
+            RuleFor(x => x.Name)
+                   .NotEmpty()
+                   .MaximumLength(25);
+
+            RuleFor(x => x.Surename)
+                .NotEmpty()
+                .MaximumLength(25);
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty();
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(e => e.Email == value);
+
+                    if (emailInUse)
+                    {
+                        context.AddFailure("Email", "Email is already taken");
+                    }
+                });
+
+            RuleFor(x => x.Password).MinimumLength(8);
+            RuleFor(x => x.ConfirmPassword).Equal(e => e.Password);
+        }
+    }
+}
