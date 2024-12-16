92727fb685ecbae93ee82cd41e61ca4d598e2d99
Abatur <alek@zolnowski.eu>
2024-12-16 23:35:40 +0100

    feat: Add nullable variables for updateUser and add JoinAt set up for Account Creation. Fix: user check in userService.

--- WrocRide/Models/UpdateUserDto.cs
+++ WrocRide/Models/UpdateUserDto.cs
@@ -2,11 +2,11 @@
 {
     public class UpdateUserDto
     {
-        public string Name { get; set; }
-        public string Surename { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+        public string? Name { get; set; }
+        public string? Surename { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
     }
 }
