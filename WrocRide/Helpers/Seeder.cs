7d8caad50db07cce08e40054b994fb7210f659c8
Aleksander <alek@zolnowski.eu>
2025-01-04 14:09:13 +0100

    feat: Add schedule-related entites and seeder to fill up days of the week

--- WrocRide/Helpers/Seeder.cs
+++ WrocRide/Helpers/Seeder.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 using WrocRide.Entities;
+using DayOfWeek = WrocRide.Entities.DayOfWeek;
 
 namespace WrocRide.Seeders
 {
@@ -38,6 +39,43 @@ namespace WrocRide.Seeders
                         context.SaveChanges();
                     }
 
+                    if (!context.DayOfWeeks.Any())
+                    {
+                        var days = new List<DayOfWeek>()
+                        {
+                            new DayOfWeek()
+                            {
+                                Day = "Monday"
+                            },
+                            new DayOfWeek()
+                            {
+                                Day = "Tuesday"
+                            },
+                            new DayOfWeek()
+                            {
+                                Day = "Wednesday"
+                            },
+                            new DayOfWeek()
+                            {
+                                Day = "Thursday"
+                            },
+                            new DayOfWeek()
+                            {
+                                Day = "Friday"
+                            },
+                            new DayOfWeek()
+                            {
+                                Day = "Saturday"
+                            },
+                            new DayOfWeek()
+                            {
+                                Day = "Sunday"
+                            },
+                        };
+                        
+                        context.DayOfWeeks.AddRange(days);
+                        context.SaveChanges();
+                    }
                 }
                 catch(Exception)
                 {
