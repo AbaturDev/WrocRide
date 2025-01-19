eb98ccc15d6acf28ddd500dff66dcc7111000a83
Abatur <alek@zolnowski.eu>
2025-01-20 00:36:03 +0100

    feat: Add user profile page with user data visualisation and deactivate account option

---
+++ WrocRide.Client/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using WrocRide.Shared.DTOs.User;
+
+namespace WrocRide.Client.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto> GetUser();
+        Task UpdateProfile(UpdateUserDto dto);
+        Task AddCredits(AddCreditsDto dto);
+        Task DeactivateAccount();
+    }
+}
